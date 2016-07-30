5aa988638491390ffe789b78eadfdb1f8b080e54
kms1837 <kms1837@gmail.com>
2016-07-30 11:54:28 +0000

    rollback commentBast check code

--- chrome/js/RuliwebSupport.js
+++ chrome/js/RuliwebSupport.js
@@ -214,8 +214,38 @@ function mypiCheck(response)
 function BoardCommentCheck(response) //blockType, checkUserList
 {
 	var commentTable	= $('.comment_view_wrapper .comment_view.normal.row tbody tr')
+	var commentBast		= $('.comment_view_wrapper .comment_view.best.row tbody tr');
 	var count = 0;
 	var logs = {};
+
+	$(commentBast).each(function(index, object) {
+ 		var writerName  = $(object).find('.user_inner_wrapper .nick a').text();
+ 		var writerID	= $(object).find('.user_inner_wrapper .member_srl').text();
+ 		var subject = object;
+ 		
+ 		writerID = writerID.substr(1, writerID.length-2);
+ 		
+ 		$(object).find('.user_inner_wrapper .nick a').contextmenu(contextMenu);
+ 		
+ 		var userInfo = {
+ 			writerName  : writerName,
+ 			writerID	: writerID
+ 		};
+ 		
+ 		var countFlag = userNodeCheck(response.data.aggrohuman, subject, userInfo);
+ 	
+ 		if(countFlag) {
+ 			var defaultInfo = {
+ 				name 	: writerName,
+ 				id 		: writerID,
+ 				count 	: 0
+ 			}
+ 			if(logs[writerName] === undefined) logs[writerName] = defaultInfo;
+ 			logs[writerName].count = parseInt(logs[writerName].count) + 1;
+ 		}
+ 
+ 		count = countFlag ? count+1 : count;
+ 	});
 	
 	$(commentTable).each(function(index, object) {
 		var writerName  = $(object).find('.user_inner_wrapper .nick a').text();
