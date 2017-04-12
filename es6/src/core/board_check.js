2cc240282b1fbace696ecc7b1eae21b68cf71451
kms1837 <kms1837@gmail.com>
2017-04-12 20:32:34 +0900

    id로 차단 작동되지 않는 문제 수정

--- es6/src/core/board_check.js
+++ es6/src/core/board_check.js
@@ -24,10 +24,11 @@ class BoardCheck
     			$(object).find('.writer a').contextmenu(Common.contextMenu);
     		}
     		
-    		let userInfo = Object.assign(Common.defaultCheckUserForm, {
+    		let userInfo = {
+                ...Common.defaultCheckUserForm,
     	        writerName  : writerName,
     			writerID	: writerID
-    	    });
+    	    };
 
     		let countFlag = Common.userNodeCheck(data, subject, userInfo);
     
@@ -44,54 +45,43 @@ class BoardCheck
     	
     	Common.displayCheckCount(countFrom);
     }//function BoadtTableCheck - 게시판 어그로 체크
+
+    static commentCheck(data, logs, object) {
+        let writerName = $(object).find('.user_inner_wrapper .nick a').text();
+        let writerID = $(object).find('.user_inner_wrapper .member_srl').text();
+        let subject = object;
+
+        writerID = writerID.replace(/[^0-9]/g, "");
+
+        $(object).find('.user_inner_wrapper .nick a').contextmenu(Common.contextMenu);
+        
+        let userInfo = {
+            ...Common.defaultCheckUserForm,
+            writerName  : writerName,
+            writerID	: writerID
+        };
+        
+        let countFlag = Common.userNodeCheck(data, subject, userInfo);
+        
+        if (countFlag) Common.logUserCounter(logs, writerName, writerID);
+
+        return countFlag;
+    }
     
     static boardCommentCheck(data) //blockType, checkUserList
     {
-    	let commentTable	= $('.comment_view_wrapper .comment_view.normal.row tbody tr')
-    	let commentBast		= $('.comment_view_wrapper .comment_view.best.row tbody tr');
+    	let commentTable = $('.comment_view_wrapper .comment_view.normal.row tbody tr')
+    	let commentBast = $('.comment_view_wrapper .comment_view.best.row tbody tr');
     	let count = 0;
     	let logs = {};
     
     	$(commentBast).each( (index, object) => {
-     		let writerName  = $(object).find('.user_inner_wrapper .nick a').text();
-     		let writerID	= $(object).find('.user_inner_wrapper .member_srl').text();
-     		let subject = object;
-     		
-     		writerID = writerID.substr(1, writerID.length-2);
-     		
-     		$(object).find('.user_inner_wrapper .nick a').contextmenu(Common.contextMenu);
-     		
-     		let userInfo = Object.assign(Common.defaultCheckUserForm, {
-    	        writerName  : writerName,
-    			writerID	: writerID
-    	    });
-     		
-     		let countFlag = Common.userNodeCheck(data, subject, userInfo);
-     		
-     		if (countFlag) Common.logUserCounter(logs, writerName, writerID);
-     
-     		count = countFlag ? count+1 : count;
-     	});
+            count = BoardCheck.commentCheck(data, logs, object) ? count + 1 : count;
+         }).bind(BoardCheck);
     	
     	$(commentTable).each( (index, object) => {
-    		let writerName  = $(object).find('.user_inner_wrapper .nick a').text();
-    		let writerID	= $(object).find('.user_inner_wrapper .member_srl').text();
-    		let subject = object;
-    		
-    		writerID = writerID.substr(1, writerID.length-2);
-    		
-    		$(object).find('.user_inner_wrapper .nick a').contextmenu(Common.contextMenu);
-    		
-    		let userInfo = Object.assign(Common.defaultCheckUserForm, {
-    	        writerName  : writerName,
-    			writerID	: writerID
-    	    });
-    	    
-    		let countFlag = Common.userNodeCheck(data, subject, userInfo);
-    		if (countFlag) Common.logUserCounter(logs, writerName, writerID);
-    
-    		count = countFlag ? count + 1 : count;
-    	});
+            count = BoardCheck.commentCheck(data, logs, object) ? count + 1 : count;
+    	}).bind(BoardCheck);
     	
     	let countFrom = {
     		title : 'comment',
