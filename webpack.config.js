4a6f1dde61072d905cbca706549f16ca8605370f
kms1837 <kms1837@gmail.com>
2017-05-20 20:19:13 +0900

    webpack 개발용 릴리즈용 파일 분할

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,5 @@
+const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
+
+module.exports = merge(commonConfig, {
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,1 +1,10 @@
 const commonConfig = require('./webpack.common.js');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+module.exports = merge(commonConfig, {
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+});
